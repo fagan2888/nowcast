d7d74e97d2889353809e41d0c7c5cdc884395be8
LSimonsen <LSimonsen@b65b143d-dad5-48b2-bbf2-81d1f728c7de>
2016-10-04 16:23:27 +0000

    Adding fixes after trying to install as a service
    git-svn-id: http://mslinuxdb01.macrosynergy.local/repositories/Nowcast/trunk@72 b65b143d-dad5-48b2-bbf2-81d1f728c7de

--- py/msMBDbService.py
+++ py/msMBDbService.py
@@ -1,6 +1,7 @@
 import win32serviceutil
 import win32service
 import win32event
+import win32api
 import servicemanager
 import socket
 import time
@@ -17,9 +18,9 @@ class msMBDbService(win32serviceutil.ServiceFramework):
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)     
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(60)
-        self.stop_requested = False      
+        self.stop_requested = False
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
@@ -29,38 +30,36 @@ class msMBDbService(win32serviceutil.ServiceFramework):
 
     def SvcDoRun(self):
         import servicemanager
-        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED(seld._svc_name_, ''))
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED(self._svc_name_, ''))
         self.timeout = 1080000
-    
-    while 1:
-         # Wait for service stop signal, if I timeout, loop again
-        rc = win32event.WaitForSingleObject(self.hWaitStop, self.timeout)
-        if rc == win32event.WAIT_OBJECT_0:
-            servicemanager.LogInfoMsg("msDbMBService Stopped")
-            break
-        else:
-            try:
-                mb_up = msMBDbInterface(user = 'dbuser', password = 'Melbourne2016', host = 'mslinuxdb01', db_name = 'ms_econ_Db_DEV')
-                indicator_updates = mb_up.available_updates()
+        while 1:
+             # Wait for service stop signal, if I timeout, loop again
+            rc = win32event.WaitForSingleObject(self.hWaitStop, self.timeout)
+            if rc == win32event.WAIT_OBJECT_0:
+                servicemanager.LogInfoMsg("msDbMBService Stopped")
+                break
+            else:
+                try:
+                    mb_up = msMBDbInterface(user = 'dbuser', password = 'Melbourne2016', host = 'mslinuxdb01', db_name = 'ms_econ_Db_DEV')
+                    indicator_updates = mb_up.available_updates()
 
-                if indicator_updates.len() > 0:
-                    logger.info("Updates found")
-                    all_series = d.FetchSeries(indicator_updates)
-                    for num, indicator_key in enumerate(all_series):
-                        ts = all_series[num]
-                        releaseName = ts.Metadata.GetFirstValue("Release")
-                        releaseEntity = d.FetchOneEntity(releaseName)
-                        current_release = releaseEntity.Metadata.GetFirstValue("LastReleaseEventTime")
-                        next_release = releaseEntity.Metadata.GetFirstValue("NextReleaseEventTime")
-                        if 'bea037_76a067rx_m' != str(indicator_key):
-                            mb_up.upload_mb_data(ts, str(indicator_key),  current_release, next_release)                        
-            except:
-                pass
+                    if indicator_updates.len() > 0:
+                        logger.info("Updates found")
+                        all_series = d.FetchSeries(indicator_updates)
+                        for num, indicator_key in enumerate(all_series):
+                            ts = all_series[num]
+                            releaseName = ts.Metadata.GetFirstValue("Release")
+                            releaseEntity = d.FetchOneEntity(releaseName)
+                            current_release = releaseEntity.Metadata.GetFirstValue("LastReleaseEventTime")
+                            next_release = releaseEntity.Metadata.GetFirstValue("NextReleaseEventTime")
+                            if 'bea037_76a067rx_m' != str(indicator_key):
+                                mb_up.upload_mb_data(ts, str(indicator_key),  current_release, next_release)
+                except:
+                    pass
 
 def ctrlHandler(ctrlType):
    return True
-                          
-if __name__ == '__main__':   
-   win32api.SetConsoleCtrlHandler(ctrlHandler, True)   
-   win32serviceutil.HandleCommandLine(msMBDbService)
 
+if __name__ == '__main__':
+   win32api.SetConsoleCtrlHandler(ctrlHandler, True)
+   win32serviceutil.HandleCommandLine(msMBDbService)
