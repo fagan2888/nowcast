ba3edcb9a165c12b37d9c573cae0d465b12f3e50
pnash <pnash@b65b143d-dad5-48b2-bbf2-81d1f728c7de>
2016-10-10 07:40:56 +0000

    Changing timestring on log file, removeing hours/mins
    git-svn-id: http://mslinuxdb01.macrosynergy.local/repositories/Nowcast/trunk@82 b65b143d-dad5-48b2-bbf2-81d1f728c7de

--- py/msLogConfig.py
+++ py/msLogConfig.py
@@ -2,7 +2,7 @@ import logging
 import time
 import socket
 
-timestr = time.strftime("%Y%m%d-%H%M%S")
+timestr = time.strftime("%Y%m%d")
 log_filename = 'W:\\Shared\\Nowcasting\\Logs\\MS_MacroB_Db_logfile_' + socket.gethostname() + '_' + timestr + '.log'
 FORMAT = '%(asctime)-15s %(funcName)s %(lineno)d %(message)s'
 logging.basicConfig(filename = log_filename, format=FORMAT, level = logging.INFO)
